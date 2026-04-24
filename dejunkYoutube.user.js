697adf66b2529d0e3d28313fe2d52747cff5f718
smkapilgupta <41782205+smkapilgupta@users.noreply.github.com>
2026-04-24 21:05:36 +0530

    Update dejunkYoutube.user.js

--- dejunkYoutube.user.js
+++ dejunkYoutube.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dejunk youtube
 // @namespace    https://github.com/smkapilgupta/siteBlocker2
-// @version      1.1.11
+// @version      1.1.12
 // @description  Script to block short format videos and youtube suggestions
 // @author       Kapil Gupta <smkapilgupta@gmail.com>
 // @match        *://*.youtube.com/*
@@ -72,7 +72,7 @@ function udpateRecords(records){
         setTimeout(()=>obliterateNode(node),0)
       })
       document.querySelectorAll(".ytd-shelf-renderer").forEach(node=>{
-        if(node.innerText.includes("People also watched") || node.innerText.includes("Previously watched"))
+        if(node.innerText.includes("People also watched") || node.innerText.includes("Previously watched") || node.innerText.includes("Explore more"))
           setTimeout(()=>obliterateNode(node),0)
     })
 
